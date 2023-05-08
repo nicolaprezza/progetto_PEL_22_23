510e43b210f5a44dfcdf3c69f894e6ca7a0cd64a
Giulio Ermanno Pibiri <jeis90@gmail.com>
2023-05-08 12:36:35 +0200

    formatting

--- json.hpp
+++ json.hpp
@@ -5,70 +5,66 @@
 #include <fstream>
 
 struct json_exception {
-	std::string msg;
+    std::string msg;
 };
 
 class json {
-
 public:
-	
-	struct list_iterator;
-	struct dictionary_iterator;
-	struct const_list_iterator;
-	struct const_dictionary_iterator;
-	
-	json();
-	json(json const&);
-	json(json&&);
-	~json();
-	
-	json& operator=(json const&);
-	json& operator=(json&&);
+    struct list_iterator;
+    struct dictionary_iterator;
+    struct const_list_iterator;
+    struct const_dictionary_iterator;
 
-	bool is_list() const;
-	bool is_dictionary() const;
-	bool is_string() const;
-	bool is_number() const;
-	bool is_bool() const;
-	bool is_null() const;
+    json();
+    json(json const&);
+    json(json&&);
+    ~json();
 
-	json const& operator[](std::string const&) const;
-	json& operator[](std::string const&);
+    json& operator=(json const&);
+    json& operator=(json&&);
 
-	list_iterator begin_list();
-	const_list_iterator begin_list() const;
-	list_iterator end_list();
-	const_list_iterator end_list() const;
+    bool is_list() const;
+    bool is_dictionary() const;
+    bool is_string() const;
+    bool is_number() const;
+    bool is_bool() const;
+    bool is_null() const;
 
-	dictionary_iterator begin_dictionary();
-	const_dictionary_iterator begin_dictionary() const;
-	dictionary_iterator end_dictionary();
-	const_dictionary_iterator end_dictionary() const;
+    json const& operator[](std::string const&) const;
+    json& operator[](std::string const&);
 
-	double& get_number();
-	double const& get_number() const;
+    list_iterator begin_list();
+    const_list_iterator begin_list() const;
+    list_iterator end_list();
+    const_list_iterator end_list() const;
 
-	bool& get_bool();
-	bool const& get_bool() const;
+    dictionary_iterator begin_dictionary();
+    const_dictionary_iterator begin_dictionary() const;
+    dictionary_iterator end_dictionary();
+    const_dictionary_iterator end_dictionary() const;
 
-	std::string& get_string();
-	std::string const& get_string() const;
+    double& get_number();
+    double const& get_number() const;
 
-	void set_string(std::string const&);
-	void set_bool(bool);
-	void set_number(double);
-	void set_null();
-	void set_list();
-	void set_dictionary();
-	void push_front(json const&);
-	void push_back(json const&);
-	void insert(std::pair<std::string,json> const&);
+    bool& get_bool();
+    bool const& get_bool() const;
+
+    std::string& get_string();
+    std::string const& get_string() const;
+
+    void set_string(std::string const&);
+    void set_bool(bool);
+    void set_number(double);
+    void set_null();
+    void set_list();
+    void set_dictionary();
+    void push_front(json const&);
+    void push_back(json const&);
+    void insert(std::pair<std::string, json> const&);
 
 private:
-		
-	struct impl;
-	impl* pimpl;
-	
+    struct impl;
+    impl* pimpl;
 };
 
 std::ostream& operator<<(std::ostream& lhs, json const& rhs);
